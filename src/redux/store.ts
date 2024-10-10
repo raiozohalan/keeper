77af3b10a53d68c72a31fa5ebd1b6d8c971806bc
Joelhy Galan <34236019+raiozohalan@users.noreply.github.com>
2024-10-10 22:44:10 +0800

    chore: setup redux persist

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,23 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { rootReducer } from './reducers'
-// ...
+import { configureStore } from "@reduxjs/toolkit";
+import { rootReducer } from "./reducers";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
 
-export const store = configureStore({
-  reducer: rootReducer,
-})
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
+export default store;
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
