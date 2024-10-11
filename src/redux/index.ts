4171cd1a5881512ef9c2a71f4eb469dd69f60aa7
Joelhy Galan <34236019+raiozohalan@users.noreply.github.com>
2024-10-11 22:29:07 +0800

    chore: add reducers for notes slice

---
+++ src/redux/index.ts
@@ -0,0 +1,7 @@
+import * as notes from "./notes";
+
+const slices = {
+  notes,
+};
+
+export default slices;
--- src/redux/notes/slice.ts
+++ src/redux/notes/slice.ts
@@ -1,15 +1,31 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { Notes } from "./interface";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Note, Notes } from "./interface";
 
 const initialState: Notes = {
   list: [],
   selectedNote: null,
 };
+
 export const slice = createSlice({
   name: "notes",
   initialState,
   reducers: {
-    // Add reducers here
+   setList: (state, { payload } : PayloadAction<Note[]>) => {
+      state.list = payload;
+    },
+    updateNote: (state, { payload } : PayloadAction<Note>) => {
+      const findNoteIndex = state.list.findIndex((note) => note.id === payload.id);
+      state.list[findNoteIndex] = payload;
+    },
+    addNote: (state, { payload } : PayloadAction<Note>) => {
+      state.list.push(payload);
+    },
+    deleteNote: (state, { payload } : PayloadAction<string>) => {
+      state.list = state.list.filter((note) => note.id !== payload);
+    },
+    setSelectedNote: (state, { payload } : PayloadAction<Note>) => {
+      state.selectedNote = payload;
+    },
   },
 });
 
