7269f3c9c7107fe68c57909ca883b25bf92441cf
Joelhy Galan <34236019+raiozohalan@users.noreply.github.com>
2024-10-15 08:39:50 +0800

    refactor: Update Header component UI and add theme selector

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,25 @@
+import Image from "next/image";
 import React from "react";
+import ThemeSelector from "../Base/ThemeSelector";
 
 const Header = () => {
   return (
-    <div className="flex-none w-full h-14 sticky top-0 bg-gray-800 z-50">
-      Header
+    <div className="flex-none w-full h-14 sticky top-0 bg-light- z-50 px-8 py-3 dark:bg-dark-primary">
+      <div className="flex gap-2 items-center">
+        <div className="w-8 h-8 rounded-md overflow-hidden">
+          <Image
+            src="/assets/keeper-logo-white-192.jpg"
+            alt="Keeper"
+            width={32}
+            height={32}
+            className=""
+          />
+        </div>
+        <h2 className="font-extrabold">KEEPER</h2>
+      </div>
+      <div className="flex gap-2 items-center">
+        <ThemeSelector />
+      </div>
     </div>
   );
 };
