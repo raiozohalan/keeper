7cffa179eabe6661d1a25738d8fc128ecb6f2b44
Joelhy Galan <34236019+raiozohalan@users.noreply.github.com>
2024-10-16 22:36:26 +0800

    refactor: add disabled button for selected theme

--- src/components/Base/ThemeSelector.tsx
+++ src/components/Base/ThemeSelector.tsx
@@ -7,16 +7,20 @@ const themes = [
   { name: "System", value: "system" },
 ];
 const ThemeSelector = () => {
-  const { theme, setTheme } = useTheme();
-  return themes.map((theme) => (
-    <button
-      key={theme.value}
-      className="px-2 py-1 text-sm font-semibold text-gray-700 bg-gray-200 rounded-md dark:bg-dark-accent2 dark:text-gray-300"
-      onClick={() => setTheme(theme.value)}
-    >
-      {theme.name}
-    </button>
-  ));
+  const { theme: currenTheme, setTheme } = useTheme();
+  return themes.map((theme) => {
+    const isCurrentTheme = theme.value === currenTheme;
+    return (
+      <button
+        key={theme.value}
+        className="px-2 py-1 text-sm font-semibold text-gray-700 bg-gray-200 rounded-md dark:bg-dark-accent2 dark:text-gray-300"
+        onClick={() => setTheme(theme.value)}
+        disabled={isCurrentTheme}
+      >
+        {theme.name}
+      </button>
+    );
+  });
 };
 
 export default ThemeSelector;
